7ff8dd441e31b4e625a8a7ada99191ee247ca254
oscar91511 <gamingsoftwarepereira@gmail.com>
2024-11-05 20:56:23 -0500

    correction on pading on navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,39 +3,39 @@ import logo from "/images/Logo_Proyecto.png";
 
 function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white flex justify-between items-center">
-      <Link to="/" className="flex items-center">
+    <nav className="bg-blue-600 text-white flex justify-between items-center p-2">
+      <Link to="/" className="flex items-center space-x-2">
         <img
           src={logo}
           alt="Logo de Ansiedad Académica"
-          className="h-20 transition duration-300 ease-in-out transform hover:scale-110"
+          className=" h-10 w-14 pe-2 md:h-16 transition duration-300 ease-in-out transform hover:scale-110"
         />
-        <span className="py-2 pe-4 text-xs sm:text-xl italic">
+        <span className="text-xs sm:text-lg md:text-xl italic">
           Podemos vencer a la mente!
         </span>
       </Link>
-      <div className="flex space-x-2 px-4">
+      <div className="flex space-x-4 md:space-x-6 px-2 md:px-2">
         <Link
           to="/"
-          className="px-1 py-2 text-white hover:bg-blue-700 rounded text-xs sm:text-base"
+          className=" py-2 text-white hover:bg-blue-700 rounded text-xs sm:text-base md:text-lg"
         >
           Inicio
         </Link>
         <Link
           to="/informacion"
-          className="px-1 py-2 text-white hover:bg-blue-700 rounded text-xs sm:text-base"
+          className=" py-2 text-white hover:bg-blue-700 rounded text-xs sm:text-base md:text-lg"
         >
           Información
         </Link>
         <Link
           to="/resources"
-          className="px-1 py-2 text-white hover:bg-blue-700 rounded text-xs sm:text-base"
+          className=" py-2 text-white hover:bg-blue-700 rounded text-xs sm:text-base md:text-lg"
         >
           Recursos
         </Link>
         <Link
           to="/contacto"
-          className="px-1 py-2 text-white hover:bg-blue-700 rounded text-xs sm:text-base"
+          className=" py-2 text-white hover:bg-blue-700 rounded text-xs sm:text-base md:text-lg"
         >
           Contacto
         </Link>
